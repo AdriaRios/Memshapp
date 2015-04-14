11f3be259b0226b7dbb5398f5750d2d0fe46dc33
Adria Rios <adrianrios89@gmail.com>
2015-04-14 20:47:34 +0200

    Memshapp - Added video and audio to Details View

--- app/src/main/java/org/adriarios/memshapp/activities/DetailsMemoryAC.java
+++ app/src/main/java/org/adriarios/memshapp/activities/DetailsMemoryAC.java
@@ -3,12 +3,19 @@ package org.adriarios.memshapp.activities;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
+import android.widget.MediaController;
 
 import org.adriarios.memshapp.R;
 import org.adriarios.memshapp.asynctask.BitmapWorkerTask;
@@ -20,6 +27,8 @@ import java.util.Locale;
 public class DetailsMemoryAC extends ActionBarActivity {
     //Data
     String mImagePath;
+    String mAudioPath;
+    String mVideoPath;
     String mTitleData;
     String mDescriptionData;
     Double mLatitude;
@@ -30,6 +39,11 @@ public class DetailsMemoryAC extends ActionBarActivity {
     TextView mTitle;
     TextView mDescription;
     TextView mAddress;
+    VideoView mVideoView;
+    Button mAudioButton;
+
+    MediaController mediaController;
+    MediaPlayer mPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,18 +54,31 @@ public class DetailsMemoryAC extends ActionBarActivity {
         mTitle = (TextView) findViewById(R.id.titleDetails);
         mDescription = (TextView) findViewById(R.id.descDetails);
         mAddress = (TextView)findViewById(R.id.addressDetails);
+        mVideoView = (VideoView)findViewById(R.id.videoDetails);
+        mAudioButton = (Button)findViewById(R.id.playAudioButtonDetails);
+        mAudioButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                playAudioRecorded();
+            }
+
+        });
         //Get Memory Info
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
 
         mImagePath = extras.getString("DETAILS_IMAGE_PATH");
         mTitleData= extras.getString("DETAILS_TITLE");
+        mAudioPath= extras.getString("DETAILS_AUDIO_PATH");
+        mVideoPath= extras.getString("DETAILS_VIDEO_PATH");
         mDescriptionData= extras.getString("DETAILS_DESCRIPTION");
         mLatitude= extras.getDouble("DETAILS_LATITUDE");
         mLongitude= extras.getDouble("DETAILS_LONGITUDE");
 
+
         mTitle.setText(mTitleData);
         mDescription.setText(mDescriptionData);
+
+        initMultimedia();
 
         BitmapWorkerTask task = new BitmapWorkerTask(mImage,mImagePath,400,150);
         task.execute();
@@ -69,6 +96,36 @@ public class DetailsMemoryAC extends ActionBarActivity {
 
     }
 
+    private void initMultimedia() {
+        Log.i("DETAILS", "video "+mVideoPath);
+        Log.i ("DETAILS", "audio "+mAudioPath);
+        if (mVideoPath==null){
+            mVideoView.setVisibility(View.GONE);
+        }else{
+            mVideoView.setVideoURI(Uri.parse(mVideoPath));
+            mediaController = new MediaController(this);
+            mediaController.setAnchorView(mVideoView);
+            mVideoView.setMediaController(mediaController);
+            mVideoView.seekTo(1);
+
+
+        }
+
+        if (mAudioPath==null){
+            mAudioButton.setVisibility(View.GONE);
+        }
+    }
+
+    private void playAudioRecorded() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mAudioPath);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            //Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
